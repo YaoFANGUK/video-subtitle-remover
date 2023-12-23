9aeb8f886025e2a4741137e83fd9c6585f3d4afb
天涯古巷 <flavioy@live.com>
2023-12-23 10:49:30 +0800

    Update sttn_inpaint.py

--- backend/inpaint/sttn_inpaint.py
+++ backend/inpaint/sttn_inpaint.py
@@ -5,6 +5,7 @@ import torch
 from torchvision import transforms
 from typing import List
 import sys
+import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from backend import config
